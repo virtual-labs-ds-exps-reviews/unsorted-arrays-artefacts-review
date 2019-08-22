a44ee4f8e7228bc3df2d17f8ea01549a872ae001
sadhana <sadhana.virupaksha@gmail.com>
2019-08-22 17:39:04 +0530

    add code

---
+++ code/runtime/js/realization.js
@@ -0,0 +1,59 @@
+
+var realizationCatalog = {
+  "Linear-Search Demo": {
+    "reqs_satisfied": "Provides an artefact to demonstrate Linear Search",
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/linear_search_demo.html"
+  },
+  "Binary-Search Demo": {
+    "reqs_satisfied": "Provides an artefact to demonstrate Binary Search",
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/binary_search_demo.html"
+  },
+  "Binary-Search Exercise": {
+    "reqs_satisfied": "Provides an artefact to exercise Binary Search",
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/binary_search_exercise.html"
+  },
+  "Binary-Search Practice": {
+    "reqs_satisfied": "Provides an artefact to practice Binary Search",
+    "resource_type": "html",
+    "url": "/build/code/runtime/html/binary_search_practice.html"
+  },
+  "Sorting Visualization": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/sorted_vs_unsorted.png",
+    "height": "60%",
+    "width": "80%"
+  },
+  "Comparing Elements": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/stop_condition_linear.png",
+    "height": "40%",
+    "width": "80%"
+  },
+  "Demonstration-Linear": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/linear_search_stepwise.png",
+    "height": "70%",
+    "width": "80%"
+  },
+  "Linear Search Visualization": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/linear_search_stepwise.png",
+    "height": "70%",
+    "width": "80%"
+  },
+  "Demonstration-Binary": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/binary_search_stepwise.png",
+    "height": "80%",
+    "width": "80%"
+  },
+  "Binary Search Visualization": {
+    "resource_type": "Image",
+    "url": "/build/code/static/img/binary_search_stepwise.png",
+    "height": "60%",
+    "width": "75%"
+  }
+};  
